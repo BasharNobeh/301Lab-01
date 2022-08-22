1a296f2f46f389a2ace06c90b5269dc7630458f1
BasharNobeh <Basharnobeh2001@gmail.com>
2022-08-22 12:46:54 +0300

    Fixed

--- 301/react-app/src/App.js
+++ 301/react-app/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Header from '/home/adduser/301/react-app/src/Components/Header.js';
-import Footer from '/home/adduser/301/react-app/src/Components/Footer.js'
-import Main from '/home/adduser/301/react-app/src/Components/Main.js';
+import Header from './Components/Header';
+import Footer from './Components/Footer'
+import Main from './Components/Main';
 
 
 class App extends React.Component{
--- 301/react-app/src/Components/HornedBeast.js
+++ 301/react-app/src/Components/HornedBeast.js
@@ -1,17 +1,22 @@
 import React from 'react';
 
 
+
 class HornedBeast extends React.Component{
   render(){
     return (
     <div> 
-           <h2> {this.props.head} </h2>  
+           <h2> {this.props.head}  </h2>  
        
-       <img src = {this.props.src} />
+       <img  style={{
+       width: '200px',
+        height: '200px',
+      }} src = {this.props.src} alt = {this.props.alt} title = {this.props.title} />
        <p>
        {this.props.dis}
         </p> 
             
+        
     </div>
     )
   }
